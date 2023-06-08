a237683decbc9eabe7efd9ed4b3eccc9acbc7d14
Thimy Kieu <thimy@bump.sh>
2023-06-08 22:31:03 +0200

    quick fix for deploy

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link pages/home.css
 //= link administrate-field-simple_markdown/application.css
 //= link administrate-field-simple_markdown/application.js
 //= link administrate-field-active_storage/application.css
