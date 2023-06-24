b339c2879757b91c77a7c390c40b2d899582b316
Thimy Kieu <thimy@bump.sh>
2023-06-24 22:29:33 +0200

    remove motor migration and fix assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link pages/home.css
+//= link main.css
 //= link administrate-field-simple_markdown/application.css
 //= link administrate-field-simple_markdown/application.js
 //= link administrate-field-active_storage/application.css
