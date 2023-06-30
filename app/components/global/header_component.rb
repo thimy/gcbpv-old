e835c61fdfab47fab812cca30a8b2bfbd67ae6c1
Thimy Kieu <thimy@bump.sh>
2023-07-01 01:38:07 +0200

    improve public pages

--- app/components/global/header_component.rb
+++ app/components/global/header_component.rb
@@ -2,7 +2,7 @@
 
 class Global::HeaderComponent < ViewComponent::Base
   attr_reader :title, :current_user
-  delegate :user_signed_in?, to: :helpers
+  delegate :user_signed_in?, :inline_svg, to: :helpers
   
   def initialize(title:, current_user:)
     @title = title
